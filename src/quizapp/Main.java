969d45599b76606551105c43523df8d177709312
Shalini-A23 <73900417+Shalini-A23@users.noreply.github.com>
2024-08-16 14:03:40 +0530

    Add files via upload

---
+++ src/quizapp/Main.java
@@ -0,0 +1,20 @@
+
+package quizapp;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import quizapp.ui.Login;
+import repositories.JsonFileRepository;
+
+
+public class Main {
+
+   
+    public static void main(String[] args) {
+        
+        new Login().setVisible(true);
+        
+    }
+
+}
